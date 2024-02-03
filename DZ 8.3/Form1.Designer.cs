d767020715458cd0440f555d69caed0f6858c585
NikitaSmetanin <NikitaSmetanin1705@gmail.com>
2024-02-03 13:20:27 +0200

    Добавьте файлы проекта.

---
+++ DZ 8.3/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace DZ_8._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EmailBox = new System.Windows.Forms.TextBox();
+            this.EnterInfo = new System.Windows.Forms.Label();
+            this.PlsEnterLogin = new System.Windows.Forms.Label();
+            this.PassowdLabel = new System.Windows.Forms.Label();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.SignInButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EmailBox
+            // 
+            this.EmailBox.Location = new System.Drawing.Point(136, 63);
+            this.EmailBox.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.EmailBox.Name = "EmailBox";
+            this.EmailBox.Size = new System.Drawing.Size(385, 34);
+            this.EmailBox.TabIndex = 0;
+            // 
+            // EnterInfo
+            // 
+            this.EnterInfo.AutoSize = true;
+            this.EnterInfo.Location = new System.Drawing.Point(36, 20);
+            this.EnterInfo.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.EnterInfo.MaximumSize = new System.Drawing.Size(20001, 2000);
+            this.EnterInfo.Name = "EnterInfo";
+            this.EnterInfo.Size = new System.Drawing.Size(503, 26);
+            this.EnterInfo.TabIndex = 1;
+            this.EnterInfo.Text = "Будь ласка, введіть ваші данні для входу чи реєстрації";
+            // 
+            // PlsEnterLogin
+            // 
+            this.PlsEnterLogin.AutoSize = true;
+            this.PlsEnterLogin.Location = new System.Drawing.Point(50, 66);
+            this.PlsEnterLogin.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.PlsEnterLogin.Name = "PlsEnterLogin";
+            this.PlsEnterLogin.Size = new System.Drawing.Size(64, 26);
+            this.PlsEnterLogin.TabIndex = 2;
+            this.PlsEnterLogin.Text = "Email:";
+            // 
+            // PassowdLabel
+            // 
+            this.PassowdLabel.AutoSize = true;
+            this.PassowdLabel.Location = new System.Drawing.Point(50, 127);
+            this.PassowdLabel.Name = "PassowdLabel";
+            this.PassowdLabel.Size = new System.Drawing.Size(87, 26);
+            this.PassowdLabel.TabIndex = 3;
+            this.PassowdLabel.Text = "Пароль:";
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(136, 124);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(385, 34);
+            this.PasswordBox.TabIndex = 4;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(50, 196);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(59, 26);
+            this.NameLabel.TabIndex = 5;
+            this.NameLabel.Text = "Ім\'я:";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(136, 188);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(385, 34);
+            this.NameBox.TabIndex = 6;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(41, 262);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(240, 38);
+            this.LoginButton.TabIndex = 7;
+            this.LoginButton.Text = "Увійти";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // SignInButton
+            // 
+            this.SignInButton.Location = new System.Drawing.Point(288, 262);
+            this.SignInButton.Name = "SignInButton";
+            this.SignInButton.Size = new System.Drawing.Size(233, 38);
+            this.SignInButton.TabIndex = 8;
+            this.SignInButton.Text = "Зареєструватися";
+            this.SignInButton.UseVisualStyleBackColor = true;
+            this.SignInButton.Click += new System.EventHandler(this.SignInButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 331);
+            this.Controls.Add(this.SignInButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.PassowdLabel);
+            this.Controls.Add(this.PlsEnterLogin);
+            this.Controls.Add(this.EnterInfo);
+            this.Controls.Add(this.EmailBox);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.Name = "Form1";
+            this.Text = "Форма реєстрації/авторизації";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox EmailBox;
+        private System.Windows.Forms.Label EnterInfo;
+        private System.Windows.Forms.Label PlsEnterLogin;
+        private System.Windows.Forms.Label PassowdLabel;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button SignInButton;
+    }
+}
+
---
+++ DZ 8.3/Form1.cs
@@ -0,0 +1,155 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace DZ_8._3
+{
+    public partial class Form1 : Form
+    {
+        DBHelper DBHelper = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DBHelper = new DBHelper(port:8889);
+        }
+        // Функція авторизації існуючого користувача
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            DataRowCollection dataRowCollection = UserRepository.getUsers(DBHelper.getMySqlConnection(),
+               this.EmailBox.Text.Trim(), this.PasswordBox.Text.Trim());
+            
+            if (dataRowCollection.Count == 0) {
+                MessageBox.Show("Невірний логін або пароль!");
+                return; 
+            } else
+            {
+                MessageBox.Show("Користувач знайдений! Вхід успішний!");
+            }
+        }
+        // Функція реєстрації нового користувача
+        private void SignInButton_Click(object sender, EventArgs e)
+        {
+            if (this.EmailBox.Text.Trim().Length == 0 || this.NameBox.Text.Trim().Length == 0 || this.PasswordBox.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Заповніть всі необхідні поля!", "Помилка", MessageBoxButtons.OK);
+                return;
+            }
+            
+            DataRowCollection dataRowCollection = UserRepository.getUserByEmail(DBHelper.getMySqlConnection(), this.EmailBox.Text.Trim());
+            
+            if (dataRowCollection.Count != 0)
+            {
+                MessageBox.Show("Користувач з таким логіном вже існує!");
+                return;
+            }                     
+
+            UserRepository.createCity(DBHelper.getMySqlConnection(), 
+                this.NameBox.Text.Trim(), this.EmailBox.Text.Trim(), this.PasswordBox.Text.Trim());
+        }
+    }
+    // Клас налаштування підключення до бази даних
+    public class DBHelper
+    {
+        private string server = "";
+        private int port = 0;
+        private string username = "";
+        private string password = "";
+        private string database = "";
+
+        MySqlConnection mySqlConnection = null;
+
+        public DBHelper(string server = "localhost",
+                        int port = 3306,
+                        string username = "root",
+                        string password = "root",
+                        string database = "world")
+        {
+            mySqlConnection = new MySqlConnection($"server={server};port={port};username={username};password={password};database={database}");
+        }
+
+        public MySqlConnection getMySqlConnection()
+        {
+            return mySqlConnection;
+        }
+    }
+    // Клас репозиторій користувача
+    public class UserRepository
+    {
+        // Функція для отримання даних з бази даних
+        private static DataRowCollection GetSelectedData(MySqlCommand mySqlCommand)
+        {
+            MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter();
+            DataTable table = new DataTable();
+
+            mySqlDataAdapter.SelectCommand = mySqlCommand;
+            mySqlDataAdapter.Fill(table);
+
+            return table.Rows;
+        }
+        // Функція для відбору користувачів з базт даних по емейлу
+        public static DataRowCollection getUserByEmail(MySqlConnection mySqlConnection, string email)
+        {
+            using (MySqlCommand mySqlCommand = new MySqlCommand(
+                $"SELECT * " +
+                $"FROM user " +
+                $"WHERE Email = '{email}'", mySqlConnection))
+            {
+                return GetSelectedData(mySqlCommand);
+            };
+        }
+        // Функція для відбору користувачів з базі даних по емейлу та паролю
+        public static DataRowCollection getUsers(MySqlConnection mySqlConnection, string email, string password)
+        {
+            using (MySqlCommand mySqlCommand = new MySqlCommand(
+                $"SELECT * " +
+                $"FROM user " +
+                $"WHERE Email = '{email}' AND Password = '{password}'", mySqlConnection))                
+            {
+                return GetSelectedData(mySqlCommand);
+            };
+        }
+        // Функція додавання користувача в бвзу даних
+        public static void createCity(MySqlConnection mySqlConnection, string name, string email, string password)
+        {
+            try
+            {
+                MySqlCommand mySqlCommand = new MySqlCommand($"INSERT INTO user (Name, Email, Password) VALUES (@name,@email,@password)", mySqlConnection);                                
+                mySqlCommand.Parameters.Add(new MySqlParameter
+                {
+                    ParameterName = "@name",
+                    MySqlDbType = MySqlDbType.VarChar,
+                    Value = name
+                });
+                mySqlCommand.Parameters.Add(new MySqlParameter
+                {
+                    ParameterName = "@email",
+                    MySqlDbType = MySqlDbType.VarChar,
+                    Value = email
+                });
+                mySqlCommand.Parameters.Add(new MySqlParameter
+                {
+                    ParameterName = "@password",
+                    MySqlDbType = MySqlDbType.VarChar,
+                    Value = password
+                });
+               
+                mySqlConnection.Open();
+                mySqlCommand.ExecuteNonQuery();
+                mySqlConnection.Close();
+                MessageBox.Show("Користувача успішно додано в базу даних!", "Повідомлення");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+        }
+    }
+}
